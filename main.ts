9a443b518d9441d9369aae708720b3b60d0e2e9b
davidliuuuuuuuu <310887@hkis.edu.hk>
2026-04-29 14:32:56 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -17,3 +17,6 @@ let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
+game.onUpdate(function () {
+    music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
+})
