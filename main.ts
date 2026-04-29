57eb9f83a9297cdc8d8cec472c3ea0b57fb1d251
Paytrola <andrewzhao016@gmail.com>
2026-04-29 14:31:57 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -17,3 +17,4 @@ let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
+music.play(music.createSong(hex`00780004080200`), music.PlaybackMode.UntilDone)
